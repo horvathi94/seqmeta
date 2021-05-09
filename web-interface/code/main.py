5247e04d6b4713357dd0d2d7d5eaf1e59ff3bd47
horvathi94 <istvan.horvath9405@gmail.com>
2021-05-09 19:57:33 +0300

    formated pages

--- web-interface/code/main.py
+++ web-interface/code/main.py
@@ -1,50 +1,73 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 
+from cursor import Cursor
+import viewdb
 
 app = Flask(__name__)
 
 @app.route("/")
-def front_page():
-   return render_template("index.html");
+def home():
+
+    html = render_template("head.html");
+    html+= render_template("index.html");
+    html+= render_template("footer.html");
+    return html;
 
 
-@app.route("/sample", methods=["GET", "POST"])
-def submit_sample():
+@app.route("/upload")
+def upload_sample():
 
-    if request.method == "GET":
-        return render_template("table.html");
-
-    sample_name = request.form["sample-name"]
-    return """
-        <p> Sample Name: {:s} </p>
-    """.format();
+    html = render_template("head.html");
+    html+= render_template("table.html");
+    html+= render_template("footer.html");
+    return html;
 
 
 @app.route("/submit", methods=["POST"])
 def submitted():
 
+
+    html = render_template("head.html");
+
     sample_name = request.form["sample-name"]
     submission_date = datetime.today().strftime("%Y-%m-%d");
 
 
-    html = "<h1> Submitted data </h1>";
+    html+= "<h1> Submitted data </h1>";
     html+= "<p> Sample name: {:s} </p>".format(sample_name);
     html+= "<p> Submission date: {:s} </p>".format(submission_date)
 
+    html+= render_template("footer.html");
     return html;
 
-from cursor import Cursor
+
+@app.route("/search")
+def search_database():
+
+    html = render_template("head.html");
+    html+= "<h2> Search the database </h2>";
+
+    html+= viewdb.database_view();
+
+    html+= render_template("footer.html");
+
+    return html;
+
 
 @app.route("/test")
 def test():
 
-    html = "<h2> This page is for running tests </h2>";
+    html = render_template("head.html");
+    html+= "<h2> This page is for running tests </h2>";
 
     cursor = Cursor();
-    raw = cursor.describe("sample_data");
+    raw = cursor.select_all("sample_data");
+    cursor.close();
 
     html+= str(raw);
+
+    html+= render_template("footer.html");
 
     return html;
 
