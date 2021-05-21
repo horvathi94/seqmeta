a39cd651c4aab66fa9da4fe248aa407439d8d223
horvathi94 <istvan.horvath9405@gmail.com>
2021-05-21 15:16:40 +0300

    cleaned up database

--- database/docker-entrypoint-initdb.d/24_view_samples_gisaid.sql
+++ database/docker-entrypoint-initdb.d/24_view_samples_gisaid.sql
@@ -3,7 +3,12 @@ CREATE VIEW view_samples_gisaid AS
 	SELECT 
 		samples.id AS sample_id,
 		samples.name AS sample_name,
-		DATE_FORMAT(samples.collection_date, "%Y-%m-%d") AS collection_date,
+		CONCAT(samples.collection_year,  
+			IF (samples.collection_month > 0 AND samples.collection_month IS NOT NULL,
+				CONCAT("-", LPAD(samples.collection_month, 2, 0), 
+					IF (samples.collection_day > 0 AND samples.collection_day IS NOT NULL,
+						CONCAT("-", LPAD(samples.collection_day, 2, 0) ), "" ) ), 
+					"") ) AS collection_date,
 
 		hosts.label AS host,
 		samples.additional_host_info AS additional_host_info,
