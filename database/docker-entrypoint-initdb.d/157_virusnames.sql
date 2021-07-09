4d39beb1441e6232b291b27df25e8633301f5c28
horvathi94 <istvan.horvath9405@gmail.com>
2021-07-09 11:20:29 +0300

    corrected virusnames start

--- database/docker-entrypoint-initdb.d/157_virusnames.sql
+++ database/docker-entrypoint-initdb.d/157_virusnames.sql
@@ -2,6 +2,6 @@ SET @table_name = "virusnames";
 CALL create_dict_table(@table_name);
 
 CALL upsert_dict_table(@table_name, 0, "gisaid", 
-	"hCoV-19{{location}}_PV_{{sample_name}}{{collection_year}}");
+	"hCoV-19{{continent}}/{{country}}_PV_{{sample_name}}/{{collection_year}}");
 CALL upsert_dict_table(@table_name, 0, "ena-isolate",
-	"Sars-CoV-2/{{host}}/{{continent}}/{{country}}-{{collection_month}}/{{collection_year}");
+	"Sars-CoV-2/{{host}}/{{continent}}/{{country}}-{{collection_month}}/{{collection_year}}");
