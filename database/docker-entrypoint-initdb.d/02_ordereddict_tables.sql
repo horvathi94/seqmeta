04ced4010d8fd7d7136926324a70234b7cc92437
horvathi94 <istvan.horvath9405@gmail.com>
2021-08-18 16:36:12 +0300

    adding documentation

--- database/docker-entrypoint-initdb.d/02_ordereddict_tables.sql
+++ database/docker-entrypoint-initdb.d/02_ordereddict_tables.sql
@@ -54,10 +54,10 @@ CREATE PROCEDURE create_ordereddict_table(
 
 		SET @create_query = CONCAT(
 			"CREATE TABLE IF NOT EXISTS `", table_name, "`(",
-				"id 				INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,",
-				"item_key		VARCHAR(200) NOT NULL,",
+				"id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,",
+				"item_key VARCHAR(200) NOT NULL,",
 				"item_value	VARCHAR(500),",
-				"indx				INT UNSIGNED);"
+				"indx INT UNSIGNED);"
 		);
 
 		PREPARE stmt FROM @create_query;
