667e67b68db9a50e20859df062c66a2cb4ba5ff7
horvathi94 <istvan.horvath9405@gmail.com>
2021-08-18 16:36:33 +0300

    adding documentation

--- database/docker-entrypoint-initdb.d/02_ordereddict_tables.sql
+++ database/docker-entrypoint-initdb.d/02_ordereddict_tables.sql
@@ -56,7 +56,7 @@ CREATE PROCEDURE create_ordereddict_table(
 			"CREATE TABLE IF NOT EXISTS `", table_name, "`(",
 				"id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,",
 				"item_key VARCHAR(200) NOT NULL,",
-				"item_value	VARCHAR(500),",
+				"item_value VARCHAR(500),",
 				"indx INT UNSIGNED);"
 		);
 
