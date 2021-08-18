e1653673826a3acde08c18691ff9db79962c3ea8
horvathi94 <istvan.horvath9405@gmail.com>
2021-08-18 16:58:05 +0300

    adding documentation

--- database/docker-entrypoint-initdb.d/500_samples_base.sql
+++ database/docker-entrypoint-initdb.d/500_samples_base.sql
@@ -1,12 +1,13 @@
-CREATE TABLE IF NOT EXISTS samples (
+/* Basic information about the samples */
+CREATE TABLE IF NOT EXISTS `samples` (
 
-	id									INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name								VARCHAR(200) UNIQUE NOT NULL,
-	`comment`						VARCHAR(200),
-	title								VARCHAR(200),
-	description					VARCHAR(1000),
-	gisaid_accession		VARCHAR(200),
-	gisaid_virusname		VARCHAR(300),
-	isolate							VARCHAR(300)
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(200) UNIQUE NOT NULL,
+	`comment` VARCHAR(200),
+	title VARCHAR(200),
+	description VARCHAR(1000),
+	gisaid_accession VARCHAR(200),
+	gisaid_virusname VARCHAR(300),
+	isolate VARCHAR(300)
 
 );
