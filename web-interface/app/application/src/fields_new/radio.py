e88aa1d8b2f0527a14295f42f9d7d445c30fea66
horvathi94 <istvan.horvath9405@gmail.com>
2021-08-20 15:14:01 +0300

    reformatting sample editor

---
+++ web-interface/app/application/src/fields_new/radio.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class RadioValue:
+
+    label: str = "N/A";
+    value: int = 0;
+    dbval: int = None;
+    dbsave: bool = None;
+
+
+class RadioList:
+
+    items = [];
+
+    @classmethod
+    def get_list(cls):
+        return cls.items;
+
+
+    @classmethod
+    def get_item_from_dbvalue(cls, db_value: int) -> RadioValue:
+        """Get item from db_value."""
+        for item in cls.items:
+            if item.dbval == db_value: return item;
+        return RadioValue();
+
+
+    @classmethod
+    def get_item_from_value(cls, value: int) -> RadioValue:
+        """Get item from value."""
+        for item in cls.items:
+            if item.value == value: return item;
+        return RadioValue();
