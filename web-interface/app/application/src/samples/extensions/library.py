d49268f680e3bceaba79527499b3987c07fadefd
horvathi94 <istvan.horvath9405@gmail.com>
2021-08-21 12:20:23 +0300

    corrected library id empty submit

--- web-interface/app/application/src/samples/extensions/library.py
+++ web-interface/app/application/src/samples/extensions/library.py
@@ -14,6 +14,8 @@ class Library(SampleExtension):
         lay = LibraryLayouts.get_item_from_value(
             entry["library_layout_paired"]);
         entry["library_layout_paired"] = lay.dbsave;
+        if entry["library_id"] == "":
+            entry["library_id"] = None;
         if entry["library_preparation_date"] == "":
             entry["library_preparation_date"] = None;
         if entry["library_id"] == "":
@@ -28,6 +30,8 @@ class Library(SampleExtension):
         lay = LibraryLayouts.get_item_from_dbvalue(
             entry["library_layout_paired"]);
         entry["library_layout_paired"] = lay.value;
+        if entry["library_id"] == None:
+            entry["library_id"] = "";
         if "insert_size" in entry:
             if entry["insert_size"] == None:
                 entry["insert_size"] = "";
