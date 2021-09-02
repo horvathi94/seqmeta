79ef7b52707b64400b65e372f56e2e59afc1fa9f
horvathi94 <istvan.horvath9405@gmail.com>
2021-09-02 12:59:09 +0300

    js importing from registered samples

--- web-interface/app/application/samples/static/js/import-data.js
+++ web-interface/app/application/samples/static/js/import-data.js
@@ -24,50 +24,33 @@ function importDataFromOtherSample(){
 		Object.keys(sampleData).filter( (className) => className != "sample_id" ).forEach( (className) => {
 		
 			inp = templateRow.getElementsByClassName(className)[0];
-			console.log(className);
-			console.log(inp.type);
-
-		});
-
-	});
 
 
-	/*
-	const selector = document.getElementById("import-from-sample");
-	const templateRow = document.getElementById("samples-editor").getElementsByClassName("template row")[0];
-	const selectorClasses = ["collector-name", "continent", "country", "geo-loc-exposure",
-		"host", "patient-status", "host-behaviour-id", "host-habitat-id",
-		"collection-device", "host-anatomical-material", "host-body-product",
-		"prior-sars-cov-2-vaccination-id", "host-health-state", "ilness-disease-outcome",
-		"sars-cov-2-diag-gene-name-1", "sars-cov-2-diag-gene-name-2",
-		"originating-lab", "submitting-lab", "sequencing-lab", "author-group",
-		"purpose-of-sampling", "purpose-of-sequencing", "sampling-strategy",
-		"sample-capture-status", "specimen-source", "sequencing-instrument", "assembly-method",
-		"library-strategy", "library-source", "library-selection"];
-	
-	const radioClasses = ["patient-gender", "prior-sars-cov-2-infection", "prior-sars-cov-2-antiviral-treat", "hospitalisation", "library-layout"];
-	let inp;
-	
 
-	fetchSampleData(selector.value).then( (sampleData) => {
+			if (inp.type == "select"){
 
-		Object.keys(sampleData).filter( (className) => className != "sample_id" ).forEach( (className) => {
-	
-			inp = templateRow.getElementsByClassName(className)[0];
+				inp.options.forEach( (opt) => {
+					if (opt.value == sampleData[className]) {
+						opt.setAttribute("selected", "selected")
+					}
+				});
 
-			if ( selectorClasses.includes(className) ){
-				inp.options[sampleData[className]].setAttribute("selected", "selected");
-			} else if ( radioClasses.includes(className) ) {
+			} else if (inp.type == "select-one") {
+				
 				Array.from(templateRow.getElementsByClassName(className)).forEach(e => {
 					if (e.value == sampleData[className]) {e.checked=true;};
 				});
+				
 			} else {
+				
+				console.log(className, sampleData[className]);
 				inp.value = sampleData[className];
+
 			}
-		
+
 		});
 
 	});
-	*/
+
 
 }
