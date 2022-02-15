ed1c23c20b45bbef08beb76fc28c2d931ecaf146
horvathi94 <istvan.horvath9405@gmail.com>
2022-02-15 16:34:45 +0200

    clean

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const cssmin = require("gulp-cssmin");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+
+
+gulp.task("styles", function(){
+
+	return gulp.src("./dev/scss/*scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer())
+		.pipe(cssmin())
+		.pipe(rename({sufix: ".min"}))
+		.pipe( gulp.dest("app/app/static/css") );
+
+});
+
+
+gulp.task("js", function(){
+	
+	return gulp.src("./dev/assets/js/*.js")
+		.pipe(uglify())
+		.pipe(gulp.dest("app/application/static/js"));
+
+});
+
+
+gulp.task("watch:styles", function () {
+    return gulp.watch("./dev/scss/*.scss", gulp.series("styles"));
+});
+
+
+gulp.task("watch:js", function() {
+	return gulp.watch("./dev/assets/js/*.js", gulp.series("js"));
+});
+
+
+gulp.task("default", gulp.parallel("watch:js", "watch:styles"));
