5683fdc15f58ba34b939f2cbd9c1e65aac19f808
horvathi94 <istvan.horvath9405@gmail.com>
2022-04-07 10:04:34 +0300

    saving samples

--- web-interface/src/database/samples.py
+++ web-interface/src/database/samples.py
@@ -4,6 +4,7 @@ from seqmeta.objects.samples.sample import Sample
 import sys
 
 
+import sys
 class SamplesTable:
 
     table_name = "samples"
@@ -16,5 +17,19 @@ class SamplesTable:
             sql = f"INSERT INTO {cls.table_name} (name, template_id) "\
                 f"VALUES (%s, %s)"
             conn = Connect()
-            sample.id = conn.execute_sql(sql, (sample.name,sample.template_id))
-        print(f"\nSaving: {sample}", file=sys.stderr)
+            sample.id = conn.execute_sql(sql,
+                                         (sample.name,sample.template_id),
+                                         last_insert=True)
+        for attr in sample.attributes.items():
+            cls.save_attribute(sample.id, attr[0], attr[1])
+
+
+    @classmethod
+    def save_attribute(cls, sample_id: int, name: str, val: str) -> None:
+        # Insert
+        sql = f"INSERT INTO `sample_attributes` "\
+            "(sample_id, name, value) VALUES (%s, %s, %s)"
+        conn = Connect()
+        conn.execute_sql(sql, (sample_id, name, val))
+
+
