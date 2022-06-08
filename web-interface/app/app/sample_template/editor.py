0cf37403bd3f1a2b52fe30f2534ab326afc24fdc
horvathi94 <istvan.horvath9405@gmail.com>
2022-06-08 16:05:09 +0300

    redesigning template editor

--- web-interface/app/app/sample_template/editor.py
+++ web-interface/app/app/sample_template/editor.py
@@ -11,10 +11,12 @@ class Editor(Page):
 
     def __post_init__(self):
         self.add_style("template-editor.css")
-        self.add_script("fields.js")
+        self.add_script("jquery.js")
+        self.add_script("jquery-ui.js")
+#        self.add_script("fields.js")
         self.add_script("parsetemplate.js")
-        self.add_script("templates-cell.js")
-        self.add_script("templates-row.js")
+#        self.add_script("templates-cell.js")
+#        self.add_script("templates-row.js")
         self.add_script("templates-editor.js")
         self.add_script("import-ena.js")
         self.add_script("import-taxonomy.js")
