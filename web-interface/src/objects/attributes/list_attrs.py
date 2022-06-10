4decb15483b41b54f2f7000f7047c2ad6108b925
horvathi94 <istvan.horvath9405@gmail.com>
2022-06-10 12:20:44 +0300

    templates editor new

--- web-interface/src/objects/attributes/list_attrs.py
+++ web-interface/src/objects/attributes/list_attrs.py
@@ -8,9 +8,10 @@ from .builtin_fields import ena
 def list_fields(which: str) -> List[AttributeField]:
     if which == "empty":
         return [AttributeField("", "")]
-    if which == "basic":
+    elif which == "basic":
         return [AttributeField(**d) for d in basic.ALL_FIELDS]
-    if which == "gisaid_assembly":
+    elif which == "gisaid_assembly":
         return [AttributeField(**d) for d in gisaid.ALL_FIELDS]
-    if which == "ena_read":
+    elif which == "ena_read":
         return [AttributeField(**d) for d in ena.ALL_FIELDS]
+    return []
